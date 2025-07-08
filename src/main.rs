b21be0a0665808e7f8f29c944b1ac5b036202e44
Adiflickzzz <adivardhan2248@gmail.com>
2025-07-09 02:40:18 +0530

    Server is single threaded now

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,7 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    // let http_request: Vec<_> = buf_reader
-    //     .lines()
-    //     .map(|result| result.unwrap())
-    //     .take_while(|line| !line.is_empty())
-    //     .collect();
-
+    
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
